dbf57624c1b4c5548fabd1ad5e1ce322324b0571
Martim <mitram790@gmail.com>
2023-01-15 15:35:22 +0000

    Removed clpfd dependency

--- prolog/f2/5.pl
+++ prolog/f2/5.pl
@@ -1,30 +1,37 @@
 :- use_module(library(clpfd)).
 % list_size(+List, ?Size) 
 % Determina o Size de List
-list_size([],0).
-list_size([_|XS],Size):- Size1 #= Size - 1 ,list_size(XS,Size1).
+list_size([],0):-!.
+list_size([_|XS],Size):-
+    list_size(XS,Size1),
+    Size is Size1 + 1.
 
 % list_sum(+List,?Sum)
 % Soma Todos os valores contidos em List
-list_sum([],0).
-list_sum([X|XS],Sum):- SumTail #= Sum - X,list_sum(XS,SumTail).
+list_sum([],0):-!.
+list_sum([X|XS],Sum):-
+    list_sum(XS,Sum1),
+    Sum is Sum1 + X.
 
 % list_prod(+List,?Prod)
 % Multiplica Todos os valores contidos em List
-list_prod([],1).
-list_prod([X|XS],Prod):- Prod #= ProdTail * X,list_prod(XS,ProdTail).
-
+list_prod([],1):-!.
+list_prod([X|XS],Prod):-
+    list_prod(XS,Prod1),
+    Prod is Prod1 * X.
 
 % inner_product(+List,+List1,?Prod)
 % Calcula o produto escalar entre os dois vetores 
-inner_product([],[],0).
-inner_product([X|XS],[Y|YS],Prod):-
-    Prod #= ProdRest + Y*X,
-    inner_product(XS,YS,ProdRest).
+inner_product([],[],0):-!.
+inner_product([X|XS],[Y|YS],Result):-
+    inner_product(XS,YS,Result1),
+    Result is X*Y + Result1.
 
 
 % count(+Elem,+List,?N).
 % Devolve em N o número de ocorrencias de Elem em List
-count(_,[],0).
-count(Elem,[Elem|XS],N):- N1 #= N -1, count(Elem,XS,N1).
-count(Elem,[X|XS],N):- Elem \= X,count(Elem,XS,N).
+count(_,[],0):-!.
+count(Elem,[Elem|XS],N):-
+    count(Elem,XS,N1),!,
+    N is N1 + 1.
+count(Elem,[_|XS],N):- count(Elem,XS,N).
